41eae5a3ce852fac6f73d63e50365a59d18a1e4f
Mark Manuel <markmanuel0317@gmail.com>
2022-02-18 13:34:39 -1200

    Deploy to Github

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>My Notes!</h1>
+     <h3>by: Mark Manuel</h3>
+    </div>
+  );
+}
+
+export default App;
